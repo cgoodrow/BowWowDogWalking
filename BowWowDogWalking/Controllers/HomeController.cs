e874d358f8d3b6e5fe27aae2a4f0c92c9376d222
Chris Goodrow <goodrow.chris4@gmail.com>
2018-02-06 22:35:11 -0500

    fixed text and email submission format

--- BowWowDogWalking/Controllers/HomeController.cs
+++ BowWowDogWalking/Controllers/HomeController.cs
@@ -23,15 +23,17 @@ namespace BowWowDogWalking.Controllers
         {
             if (ModelState.IsValid)
             {
-                var body = "<p>Name: {0}</p> <p>Email: {1}</p> <p> Phone: {2} </p> <p>Address: {3} </p> <p>Schedule Date: {4}</p> <p>Hour: {5}</p> <p>Half Hour: {6}</p>";
+                var body = "Name: {0} <br />  Email: {1} <br /> Phone: {2} <br />  Address: {3} <br /> Schedule Date: {4} <br /> Hour: {5} <br /> Half Hour: {6}";
                 var message = new SendGridMessage();
                 message.AddTo("jessicamclark1988@gmail.com");  // replace with valid value 
                 message.From = new MailAddress("jessicamclark1988@gmail.com");  // replace with valid value
                 message.Subject = "Dog Walking Sign Up";
-                message.Text = string.Format(body,  model.Name, model.Email, model.Phone, model.Address, model.ScheduleDate, model.SixtyMin, model.ThirtyMin);
+                message.Html = string.Format(body ,model.Name, model.Email, model.Phone, model.Address, model.ScheduleDate, model.SixtyMin, model.ThirtyMin);
+                //Azure credentials
                 var username = "azure_8b8a64638c6bdacad86023f15c2e402b@azure.com";
                 var pswd = "Cg090482?";
-
+                
+                // variable to store azure credentials
                 var credentials = new NetworkCredential(username, pswd);
                 // Create an Web transport for sending email.
                 var transportWeb = new Web(credentials);
@@ -40,7 +42,7 @@ namespace BowWowDogWalking.Controllers
                 transportWeb.DeliverAsync(message);
 
                 ViewBag.Message = "Message Sent";
-                ModelState.Clear();
+                ModelState.Clear(); //clears form when page reload
                 return View("Index");
 
             }
